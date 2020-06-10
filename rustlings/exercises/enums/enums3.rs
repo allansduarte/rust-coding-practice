bd367df62552934c196ead4562b5cd86e6f56764
Allan <allan.sduarte@gmail.com>
2020-06-10 02:36:19 -0300

    Add Enum exercises solution

--- rustlings/exercises/enums/enums3.rs
+++ rustlings/exercises/enums/enums3.rs
@@ -1,21 +1,24 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
+// I AM DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    Quit,
+    Echo(String),
+    Move(Point),
+    ChangeColor(u32, u32, u32),
 }
 
 struct Point {
     x: u8,
-    y: u8
+    y: u8,
 }
 
 struct State {
     color: (u8, u8, u8),
     position: Point,
-    quit: bool
+    quit: bool,
 }
 
 impl State {
@@ -36,7 +39,17 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(r, g, b) => {
+                let r = r as u8;
+                let g = g as u8;
+                let b = b as u8;
+                self.change_color((r, g, b))
+            },
+            Message::Echo(String) => self.echo(String),
+            Message::Move(point) => self.move_position(point),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
@@ -46,14 +59,14 @@ mod tests {
 
     #[test]
     fn test_match_message_call() {
-        let mut state = State{
+        let mut state = State {
             quit: false,
-            position: Point{ x: 0, y: 0 },
-            color: (0, 0, 0)
+            position: Point { x: 0, y: 0 },
+            color: (0, 0, 0),
         };
         state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
-        state.process(Message::Move(Point{ x: 10, y: 15 }));
+        state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
 
         assert_eq!(state.color, (255, 0, 255));
@@ -61,5 +74,4 @@ mod tests {
         assert_eq!(state.position.y, 15);
         assert_eq!(state.quit, true);
     }
-
 }
